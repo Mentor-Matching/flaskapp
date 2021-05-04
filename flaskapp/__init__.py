f14c8799747f5e6171567f75fac4c64efa872138
Koo Ha Kwon <67335851+kwonkh0424@users.noreply.github.com>
2021-05-04 11:45:59 -0700

    Import modules and Configure flask

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__, static_url_path='')
+app.config['SECRET_KEY'] = 'mango213diojfeklvkl34l234l' #kk
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #kk
+db = SQLAlchemy(app) #kk
+bcrypt = Bcrypt(app) #kk
+login_manager = LoginManager(app) #kk
+login_manager.login_view = 'login' #kk
 
 from flaskapp import routes
