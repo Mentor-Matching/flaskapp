ffb271b139ddc4d8b6368af6897d4662ee23dee4
Koo Ha Kwon <67335851+kwonkh0424@users.noreply.github.com>
2021-05-04 11:48:22 -0700

    Update routes.py
    Import modules
    Create registration endpoint
    Create review endpoint

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,5 +1,10 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, flash, redirect, request
+from flaskapp import app, db, bcrypt
+from flaskapp.forms import RegistrationForm, LoginForm
+from flaskapp.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
 
 '''
 Page rendering
@@ -30,13 +35,28 @@ Landing page
 '''
 Sign up page
 '''
+@app.route("/registration", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
 
+        flash(f'Your Account has been created!', 'success')
+        return redirect(url_for('login'))
+    return render_template('index.html', title='Registeration', form=form) # This needs to be updated
 
 
 '''
 Recommendation page
 '''
-
+@app.route('/reviews', methods=['GET']) #Kooha
+def reviews():
+  return render_template('index.html')
 
 
 '''
