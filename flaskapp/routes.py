cf6b32c75d32dd0ef91432618b081303ce7cb269
Koo Ha Kwon <67335851+kwonkh0424@users.noreply.github.com>
2021-05-06 17:35:45 -0700

    Kooha 050421 (#4)
    * updated
    
    * Try pyodbc
    
    * regular update
    
    * implemented register2 page
    
    * created requirement.txt
    
    Co-authored-by: Jae Park <jaeepark@gmail.com>

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,9 +1,14 @@
+import secrets
+import os
 from flask import render_template, url_for, flash, redirect, request
+
 from flaskapp import app, db, bcrypt
-from flaskapp.forms import RegistrationForm, LoginForm
+from flaskapp.forms import RegistrationForm, LoginForm, InfoForm
 from flaskapp.models import User, Profile, Review
 from flask_login import login_user, current_user, logout_user, login_required
 import pymysql
+
+
 
 
 '''
@@ -29,14 +34,36 @@ def reviews():
 '''
 Landing page  
 '''
+# Sample Data
+mentor = [
+    {
+        'name': '서달미',
+        'title': '삼산텍 CEO',
+        'job_content': '채용, 투자유치, IR등'
+    },
+    {
+        'name': '남도산',
+        'title': '삼산텍 CTO',
+        'job_content': '기술 및 개발 총괄 리드'
+    }
+]
+
+@app.route('/api/recommendations/sample', methods=['GET'])
+def landing():
+
+  return render_template('landing.html', mentor=mentor)
 
 
+
+# @app.route('/api/recommendations?user_id=12389752179', methods=['GET'])
+# def reviews():
+#   return render_template('index.html')
 
 '''
 Sign up page
 '''
 
-@app.route("/api/profile/registration", methods=[ 'POST'])
+@app.route("/api/profile/registration", methods=[ 'GET','POST'])
 def registration():
     if current_user.is_authenticated:
         return 'Already Logged In' #redirect(url_for('home'))
@@ -50,18 +77,83 @@ def registration():
         db.session.commit()
 
         flash(f'Your Account has been created!', 'success')
-        return  'User Created: \n username: {} \n email: {}'.format(user.username, user.email)  # redirect(url_for('login'))
+        return redirect(url_for('info'))
     
-    return 'Either Username or Email already exists'
-    #return render_template('index.html', title='Registeration', form=form) # This needs to be updated
+    return render_template('register.html', title='Register', form=form)
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route("/api/profile/info", methods=[ 'GET','POST'])
+def info():
+    
+    form = InfoForm()
+    if form.submit(): #need to change this to validate_on_submit...
+      if form.picture.data:
+        a=1
+      else:
+        return render_template('info.html', title='Register', form=form)
+      image_file = save_picture(form.picture.data)
+      user_id = User.query.order_by(User.id.desc()).first() #bring the last added user
+      profile = Profile(image_file = form.image_file.data, name = form.name.data, age = form.age.data,
+      cell_phone = form.cell_phone.data, school = form.school.data, user_id = user_id)
+      db.session.add(profile)
+      db.session.commit()
+
+      return redirect(url_for('login'))
+  
+    return render_template('info.html', title='Register', form=form)
+
+'''
+Account Page
+'''
+
+@app.route("/account", methods=['GET'])
+def account():
+  filename = 'profile_pics/profile.png'
+  return render_template('account.html', title='Account', filename=filename)
 '''
 Recommendation page
 '''
 @app.route('/api/reviews', methods=['GET']) #Kooha
 def api_reviews():
   return render_template('index.html')
+
+
+'''
+Login Page: Only For Development Purpose
+'''
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return render_template('logged_in.html', title='Logged_In')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(url_for('landing')) 
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return  render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login')) 
+
 
 
 '''
@@ -82,6 +174,7 @@ def dbTestConnection():
           cursor.execute("SELECT VERSION()")
           result = cursor.fetchone()
   return result
+
 
 @app.route('/test', methods=['GET'])
 def test():
